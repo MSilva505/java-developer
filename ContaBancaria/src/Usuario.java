8d24a825362f97eb83153fc46491a8caf01f4230
MSilva505 <ftsoffline606@gmail.com>
2024-11-09 20:23:02 -0400

    atualizando

---
+++ ContaBancaria/src/Usuario.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Usuario {
+
+   
+    public static void main(String[] args) throws Exception {
+        int conta;
+        double saldo;
+        String nomeDoCliente;
+        String agencia;
+        Scanner scanner = new Scanner(System.in);
+        contaTerminal contaTerminal = new contaTerminal();
+        System.out.println("qual a sua conta?");
+        nomeDoCliente = Scanner.nextLine();
+    }
+}
---
+++ ContaBancaria/src/contaTerminal.java
@@ -0,0 +1,9 @@
+
+public class contaTerminal {
+
+String nomeDoCliente = "Marcelo Da silva";
+String agencia =" 1883-19";
+Double saldo = 62364.42;
+int conta = 5512;
+
+}
